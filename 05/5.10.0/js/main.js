03f54dd0ffc148b23ad570089a06bb1601a3baa3
Snedden Gonsalves <Snedden.Gonsalves@thomsonreuters.com>
2018-11-06 15:41:50 -0500

    Finished work

--- 05/5.10.0/js/main.js
+++ 05/5.10.0/js/main.js
@@ -29,26 +29,34 @@ var xAxisGroup = g.append("g")
     .attr("transform", "translate(0," + height +")");
 
 var yAxisGroup = g.append("g")
-    .attr("class", "y axis");								 
+    .attr("class", "y axis");
+
+var ageLabel = g.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("x",-height/2)
+    .attr("y", -20)
+    .attr("font-size",20)
+    .text("Age")
+
+
+var incomeLabel =g.append("text")
+    .attr("x", width/2)
+    .attr("y", height +50)
+    .attr("font-size",20)
+    .text("GDP")
+
+var yearLabel = g.append("text")
+    .attr("x", width - 50)
+    .attr("y", height - 50)
+    .attr("font-size", 25)
+
+
 
 d3.json("data/data.json").then(function(data){
-	   // Clean data
-	   /*
-    const formattedData = data.map(function(year){
-        return year["countries"].filter(function(country){
-            var dataExists = (country.income && country.life_exp);
-            return dataExists
-        }).map(function(country){
-            country.income = +country.income;
-            country.life_exp = +country.life_exp;
-            return country;            
-        })
-    });*/
+
 	// Clean data
-	const formattedData = data.map((year) => {
-
-
-			var d = year["countries"].filter((country) => {
+    const formattedData = data.map((year) => {
+	        var d = year["countries"].filter((country) => {
 	  		var dataExists =  country.income && country.population 
 	  		return dataExists;
 	  	}).map((country) => {
@@ -65,62 +73,60 @@ d3.json("data/data.json").then(function(data){
 	  	return yearData; 
 	});
 
-	  var maxIncome = d3.max(formattedData , (yearData) =>{
-											return d3.max(yearData.countries, (d) => {
-												//console.log("yearData ", d)
-												return d.income
-											})
-										});
-	  var minIncome = d3.min(formattedData , (yearData) =>{
-											return d3.min(yearData.countries, (d) => {
-												//console.log("yearData ", d)
-												return d.income
-											})
-										});
+    var maxIncome = d3.max(formattedData , (yearData) => {
+                                return d3.max(yearData.countries, (d) => {
+                                    //console.log("yearData ", d)
+                                    return d.income
+                                })
+                            });
+    var minIncome = d3.min(formattedData , (yearData) =>{
+                                    return d3.min(yearData.countries, (d) => {
+                                        //console.log("yearData ", d)
+                                        return d.income
+                                    })
+                                });
 
-	  var maxPopulation = d3.max(formattedData , (yearData) =>{
-											return d3.max(yearData.countries, (d) => {
-												//console.log("yearData ", d)
-												return d.population
-											})
-										});
+    var maxPopulation = d3.max(formattedData , (yearData) =>{
+                                    return d3.max(yearData.countries, (d) => {
+                                        //console.log("yearData ", d)
+                                        return d.population
+                                    })
+                                });
 
-	  var maxlifeExpt = d3.max(formattedData , (yearData) =>{
-											return d3.max(yearData.countries, (d) => {
-												//console.log("yearData ", d)
-												return d.life_exp
-											})
-										});
+    var maxlifeExpt = d3.max(formattedData , (yearData) =>{
+                                    return d3.max(yearData.countries, (d) => {
+                                            //console.log("yearData ", d)
+                                            return d.life_exp
+                                        })
+                                    });
 
-	  console.log("Max ",maxIncome, " Min", minIncome)
+
 
 	  console.log("formattedData ",formattedData);
-	  console.log("formattedData ",formattedData[18]);
-	  var populationScale = d3.scaleLinear()
-								.domain([0, maxPopulation])
-								.range([0,width ]);
 
-  //Scales
-	var gdpScale = d3.scaleLog()
+    //Scales
+    var populationScale = d3.scaleLinear()
+                            .domain([0, maxPopulation])
+                            .range([0,width ]);
+
+    var gdpScale = d3.scaleLog()
 		.base(10)
 		.domain([minIncome, maxIncome])
 		.range([0,width ]);
 
-	var x = d3.scaleLog()
-		.base(10)
-		.range([0, width])
-		.domain([142, 150000]);
-
-	var lifeExpScale = d3.scaleLinear()
+    var lifeExpScale = d3.scaleLinear()
 		.domain([0,maxlifeExpt])
 		.range([height,0])
-
-
-	 var xAxis = d3.axisBottom(gdpScale)
-		.tickFormat((d) => {
+    //Axis
+    var xAxis = d3.axisBottom(gdpScale)
+        .tickFormat((d) => {
 			return d + "$";
 		})
 		.tickValues([200,1000,8000,20000,182668]);
+    xAxisGroup.call(xAxis);
+    var yAxis = d3.axisLeft(lifeExpScale);
+    yAxisGroup.call(yAxis);
+    //Other Data
 	var numberOfCountries = 100;
 	var countries = formattedData[0].countries.map((d) => {
 		return d.country;
@@ -130,28 +136,11 @@ d3.json("data/data.json").then(function(data){
         if(!continents.includes(d.continent)) {
         	continents.push(d.continent);
 		}
-	})
-	console.log("c" , continents);
-    console.log("Color scale ", d3.interpolateRainbow);
+	});
+
 	var countryColorScale = d3.scaleOrdinal()
 		.domain(continents)
         .range(d3.schemePastel1)
-
-
-//
-	console.log("Color scale ", );
- xAxisGroup.call(xAxis);
-
- var yAxis = d3.axisLeft(lifeExpScale);
- yAxisGroup.call(yAxis);				
-
-
- 								
-
-
- 
-
-
 
     var countryLegends = g.append("g")
         .attr("id", "countryLegends")
@@ -161,9 +150,7 @@ d3.json("data/data.json").then(function(data){
         .enter()
         .append("g");
 
-
-
-    countryLegends.append("rect")
+	countryLegends.append("rect")
 			.attr("width", 10)
 			.attr("x",0)
         	.attr("y",(d,i) => (i*15 - 10))
@@ -178,13 +165,12 @@ d3.json("data/data.json").then(function(data){
     var i = 0;
 
     d3.interval(() => {
-
         update(formattedData[i]);
         i++;
-
-    },100)
+        },100);
 
     function update(yearData){
+        yearLabel.text(yearData.year);
         var t = d3.transition()
             .duration(100);
         console.log("yearData ",yearData);
